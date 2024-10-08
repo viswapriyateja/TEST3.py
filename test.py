7be897dec4feb544dc423834e0440782c427a526
viswapriyateja <161931970+viswapriyateja@users.noreply.github.com>
2024-10-07 22:56:52 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,55 +1,31 @@
-# sample program
-import unittest
+name: Python application
 
-def multiply(a, b):
+on: [push, pull_request]
 
-   return a * b
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    strategy:
+      matrix:
+        python-version: ["3.10"]
 
+    steps:
+      - uses: actions/checkout@v3
+      - name: Set up Python ${{ matrix.python-version }}
+        uses: actions/setup-python@v4
+        with:
+          python-version: ${{ matrix.python-version }}
+      - name: Install dependencies
+        run: |
+          python -m pip install --upgrade pip
+          if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
+      - name: install unit test
+        run: |
+          pip install unittest=3.12
+          ruff --format=github --target-version=py310 .
+        continue-on-error: true
 
-
-
-class MultiplyTestCase(unittest.TestCase):
-
-   def test_1(self):
-
-       result = multiply(3, 4)
-
-       self.assertEqual(result, 12)
-
-   def test_2(self):
-
-       result = multiply(3, -4)
-
-       self.assertEqual(result, -12)
-
-   def test_3(self):
-
-       result = multiply(-3, -4)
-
-       self.assertEqual(result, 12)
-       
-   def test_4(self):
-
-       result = multiply(3, 0)
-
-       self.assertEqual(result, 0)
-
-
-   def test_5(self):
-
-       result = multiply(-3, 0)
-
-       self.assertEqual(result,0)
-       
-   def test_6(self):
-
-        result = multiply(3, 1)
-
-        self.assertEqual(result,3)
-       
-       
-    
-
-       
-if __name__ == '__main__':
-    unittest.main()
+      - name: Run tests
+        run: |
+          python -m unittest test.py
+      
